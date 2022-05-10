52c0e389114f3990129af2671fe71bc271abc9cb
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-10 14:39:31 -0300

    feat(call-to-action): add call to action
    This commit add call to action html structure

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,16 @@
         <li>Blog</li>
       </ul>
     </nav>
-    <section>Call to Action</section>
+    <section class="bg-gray-100 p-10">
+      <p class="mb-5">
+        We are a team of developers with houndred hours spend on coding, we
+        create professional apps,
+      </p>
+      <form>
+        <input class="p-2 w-300" placeholder="Enter your email to subscribe" />
+        <button class="p-2 bg-green-500 text-white">Subscribe</button>
+      </form>
+    </section>
     <article v-for="article in listArticles" :key="article.id" class="my-20">
       <h2 class="text-5xl font-semibold mb-5">{{ article.title }}</h2>
       <p class="mb-5">{{ article.body }}</p>
