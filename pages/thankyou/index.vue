4a67c438e481af297c22c57336b2547d7cebb110
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-12 19:32:54 -0300

    refactor: update padding value of footer element

--- pages/thankyou/index.vue
+++ pages/thankyou/index.vue
@@ -6,11 +6,11 @@
     <nav class="p-5">
       <NuxtLink to="/">Home</NuxtLink>
     </nav>
-    <main class="my-20 bg-green-500 p-20 h-1/2">
+    <main class="my-20 bg-green-500 p-20">
       <h2 class="text-5xl text-white font-semibold mb-5">{{ greetings }}</h2>
       <p class="mb-5 text-white">{{ headline }}</p>
     </main>
-    <footer class="pb-5">
+    <footer class="pb-10">
       <h3 class="text-1xl font-semibold text-black-600">
         Nuxt Static Blog Developed By xgeovanedamasceno
       </h3>
