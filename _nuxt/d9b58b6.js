b4f41fa7bc43bde7fbe4dbc4e084f9b6cc854a92
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-13 06:55:44 -0300

    2a6a558

---
+++ _nuxt/d9b58b6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{178:function(n,o,w){w(179),n.exports=w(180)}},[[178,6,1,7]]]);
---
+++ _nuxt/static/1652435715/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffrontend-nuxtjs-static-blog\u002F_nuxt\u002Fstatic\u002F1652435715",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffrontend-nuxtjs-static-blog\u002F",assetsPath:"\u002Ffrontend-nuxtjs-static-blog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
