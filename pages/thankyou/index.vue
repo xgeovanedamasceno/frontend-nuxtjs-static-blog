dfa79364ad6052362789ba718016ee363bc70cb2
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-12 22:57:19 -0300

    feat(mobile): add mobile design layout

--- pages/thankyou/index.vue
+++ pages/thankyou/index.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="w-1/2 mx-auto">
-    <header class="mt-10 mb-5">
-      <h1 class="text-3xl font-semibold text-purple-600">{{ pageTitle }}</h1>
+  <div class="w-2/3 mx-auto">
+    <header class="mt-20 mb-5">
+      <h1
+        class="text-center text-3xl font-semibold text-purple-600 md:text-left"
+      >
+        {{ pageTitle }}
+      </h1>
     </header>
-    <nav class="p-5">
+    <nav class="p-5 text-center md:text-left">
       <NuxtLink to="/">Home</NuxtLink>
     </nav>
     <main class="my-20 bg-green-500 p-20">
-      <h2 class="text-5xl text-white font-semibold mb-5">{{ greetings }}</h2>
-      <p class="mb-5 text-white">{{ headline }}</p>
+      <h2 class="text-3xl md:text-5xl text-white font-semibold mb-5">
+        {{ greetings }}
+      </h2>
+      <p class="text-center sm:text-left mb-5 text-white">{{ headline }}</p>
     </main>
-    <footer class="pb-10">
-      <h3 class="text-1xl font-semibold text-black-600">
+    <footer class="pb-5">
+      <h3
+        class="text-center text-1xl font-semibold text-black-600 sm:text-left"
+      >
         Nuxt Static Blog Developed By xgeovanedamasceno
       </h3>
     </footer>
